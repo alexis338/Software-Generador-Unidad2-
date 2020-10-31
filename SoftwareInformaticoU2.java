a769fd2dbd06dc5bdb8501f75bd319cd67cb2cc1
alexis338 <56937435+alexis338@users.noreply.github.com>
2020-10-31 12:48:11 -0500

    Add files via upload
    Integrantes:
    - Balam Pech Julian Alejandro
    - Caamal Pech Alexis Antonio
    - Nahuat Noh Jose Eduin

---
+++ SoftwareInformaticoU2.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package softwareinformaticou2;
+
+/**
+ *
+ * @author ALEXIS
+ */
+public class SoftwareInformaticoU2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        SoftwareGenerador x = new SoftwareGenerador();
+         x.setVisible(true);
+        
+    }
+    
+}
